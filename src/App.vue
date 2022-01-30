7db1a6101e46da9de6672f3f33177c088e086f27
vicky lu <vicky.lu@IC-c02fq6m5md6r.attlocal.net>
2022-01-30 13:21:13 -0500

    vuex module build

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <h1>{{this.breeds}}</h1>
     <router-view/>
   </div>
 </template>
+
+<script>
+import {mapActions, mapState} from 'vuex';
+
+export default {
+  data(){
+    return{
+      maxQuestionRequest: 20,
+    }
+  },
+  async created(){
+    this.loadBreeds();
+    this.loadRandomFact();
+    const selectedQuestion = await this.processQuestion();
+    this.addToAnsweredFacts(selectedQuestion);
+  },
+  methods:{
+    ...mapActions(['loadBreeds', 'loadRandomFact', 'getQuestions', 'addToAnsweredFacts']),
+    async processQuestion(attempt=0){
+      if(attempt < this.maxQuestionRequest){
+          const questionsArr = await this.getQuestions();
+        // console.log(questionsArr[0]);
+
+        if(questionsArr.length){
+          // logic for selected question
+          console.log('first try');
+          return questionsArr[0];
+        }else{
+          console.log('not first try');
+          return await this.processQuestion(attempt+1);
+        }
+      }
+    }
+  },
+  computed:{
+    ...mapState(['answeredFacts']),
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,68 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex)
 
+const baseURL = "https://catfact.ninja/";
+
 export default new Vuex.Store({
   state: {
+    breeds: null,
+    firstName: null,
+    score: 7,
+    answeredFacts: [],
+    randomFact: null,
   },
   mutations: {
+    setBreeds(state, data){
+      Vue.set(state, 'breeds', data);
+    },
+    setRandomFact(state, data){
+      Vue.set(state, 'randomFact', data);
+    }
   },
   actions: {
+    async loadBreeds({commit}){
+
+      // the Merkle way
+      try{
+        const response = await axios.get(`${baseURL}breeds`);
+        commit('setBreeds', response.data.fact);
+      }catch(err) {
+        console.log(err);
+      }
+
+      // the Gongos way
+      // axios.get(`${baseURL}breeds`).then(response => {
+      //   commit('setBreeds', response.data.data);
+      // })
+    },
+
+    async loadRandomFact({commit}){
+      try{
+        const response = await axios.get(`${baseURL}fact`);
+        commit('setRandomFact', response.data.fact);
+        // console.log(response, response.data.fact);
+      }catch(err){
+        console.log(err);
+      }
+    },
+    async getQuestions({state}){
+      try{
+        const response = await axios.get(`${baseURL}facts`);
+        // console.log(response.data.data);
+        return response.data.data
+          .filter(item => item.fact.split(' ').filter(_item => !isNaN(_item)).length)
+          .filter(item => !state.answeredFacts.map(item=>item.fact).includes(item.fact));
+      }catch(err){
+        console.log(err);
+        return [];
+      }
+    }, 
+    addToAnsweredFacts({state}, data){
+      state.answeredFacts.push(data);
+    }
   },
   modules: {
   }
